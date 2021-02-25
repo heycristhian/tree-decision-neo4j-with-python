64e433c8dad55ebe17663f1503cd1094135bd689
Cristhian Dias <heycristhian@pop-os.localdomain>
2021-02-25 02:17:33 -0300

    Versao 1 finalizado

--- example.py
+++ example.py
@@ -1,6 +1,4 @@
-import logging
 from neo4j import GraphDatabase
-from neo4j.exceptions import ServiceUnavailable
 
 
 class App:
@@ -8,54 +6,8 @@ class App:
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
         self.node = {}
 
-
     def close(self):
         self.driver.close()
-
-    def create_friendship(self, person1_name, person2_name):
-        with self.driver.session() as session:
-            result = session.write_transaction(
-                self._create_and_return_friendship, person1_name, person2_name)
-            for record in result:
-                print("Created friendship between: {p1}, {p2}".format(
-                    p1=record['p1'], p2=record['p2']))
-
-    @staticmethod
-    def _create_and_return_friendship(tx, person1_name, person2_name):
-        query = (
-            "CREATE (p1:Person { name: $person1_name }) "
-            "CREATE (p2:Person { name: $person2_name }) "
-            "CREATE (p1)-[:KNOWS]->(p2) "
-            "RETURN p1, p2"
-        )
-        result = tx.run(query, person1_name=person1_name, person2_name=person2_name)
-        print('result')
-        for record in result:
-            print(record)
-        try:
-            return [{"p1": record["p1"]["name"], "p2": record["p2"]["name"]}
-                    for record in result]
-
-        except ServiceUnavailable as exception:
-            logging.error("{query} raised an error: \n {exception}".format(
-                query=query, exception=exception))
-            raise
-
-    def find_person(self, person_name):
-        with self.driver.session() as session:
-            result = session.read_transaction(self._find_and_return_person, person_name)
-            for record in result:
-                print("Found person: {record}".format(record=record))
-
-    @staticmethod
-    def _find_and_return_person(tx, person_name):
-        query = (
-            "MATCH (p:Person) "
-            "WHERE p.name = $person_name "
-            "RETURN p.name AS name"
-        )
-        result = tx.run(query, person_name=person_name)
-        return [record["name"] for record in result]
 
     @staticmethod
     def _find_node_by_id(tx, id):
@@ -68,19 +20,26 @@ class App:
         result = [record['n'] for record in result]
         return result
 
-    def execute_decision_tree(self, id, number):
+    def execute_decision_tree(self, tree_id, facts):
         with self.driver.session() as session:
-            tree = session.read_transaction(self._find_node_by_id, id)
+            tree = session.read_transaction(self._find_node_by_id, tree_id)
             if tree:
                 next_node = tree[0]['next_node']
                 node = session.read_transaction(self._find_node_by_id, next_node)
-                return self.decision_path(node[0], number)
+                return self.decision_path(node[0], facts)
             return None
 
-    def decision_path(self, node, qtde):
+    def decision_path(self, node, facts):
         with self.driver.session() as session:
+            parameter_names = node['parameter_names']
+
             if node['end_node']:
                 return node['response']
+
+            for param in parameter_names:
+                temp = param
+                vars()[temp] = facts[param]
+
             if eval(node['expression']):
                 id_node = node['node_when_true']
             else:
@@ -88,29 +47,24 @@ class App:
 
             result = session.read_transaction(self._find_node_by_id, id_node)
             result = [record for record in result]
+            return self.decision_path(result[0], facts)
 
-            return self.decision_path(result[0], qtde)
 
-    @staticmethod
-    def _get_nodes(tx):
-        venda_nova = 'venda_nova'
-        quantidade_vidas = '$quantidade_vidas'
+def bar_test():
+    parameters = {
+        'gender': 'female',
+        'age': 20
+    }
+    result = app.execute_decision_tree('bar entrance', parameters)
+    return result
 
-        query = """
-            MATCH (tree:Tree)-[r:HAS]->(rule:Rule)
-            WHERE tree.id = 'venda_nova' AND rule.id = 'quantidade_vidas'
-            return tree, rule, r
-        """
 
-        result = tx.run(query, venda_nova=venda_nova, quantidade_vidas=quantidade_vidas)
-        print('_get_nodes')
-        print(type(result))
-        result = [record for record in result]
-        return result[0]
-
-    def decision_answer(self, tree, args):
-        with self.driver.session() as session:
-            pass
+def venda_test():
+    parameters = {
+        'qtde': 31
+    }
+    result = app.execute_decision_tree('venda_nova', parameters)
+    return result
 
 
 if __name__ == "__main__":
@@ -121,8 +75,7 @@ if __name__ == "__main__":
     user = "neo4j"
     password = "4321"
     app = App(url, user, password)
-    # app.create_friendship("Alice", "David")
-    app.find_person("Alice")
-    response = app.execute_decision_tree('venda_nova', 10)
+    # response = bar_test()
+    response = venda_test()
     print(f'Result is: {response}')
     app.close()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from db.connection import connect
-from util.queries import bradesco_portabilidade
 
 session = connect()
 
@@ -8,11 +7,11 @@ MATCH (n) DETACH DELETE n
 """
 
 query1 = """
-CREATE (tree:Tree { id: 'bar entrance' })
-CREATE (over21_rule:Rule { name: 'Over 21?', parameter_names: 'age', parameter_types:'int', expression:'age >= 21' })
-CREATE (gender_rule:Rule { name: 'Over 18 and female', parameter_names: 'age,gender', parameter_types:'int,String', expression:'(age >= 18) && gender.equals(\\"female\\")' })
-CREATE (answer_yes:Answer { id: 'yes'})
-CREATE (answer_no:Answer { id: 'no'})
+CREATE (tree:Tree { id: 'bar entrance', next_node: 'over21_rule' })
+CREATE (over21_rule:Rule { id: 'over21_rule',name: 'Over 21?', parameter_names: ['age'], parameter_types:'int', expression:'age >= 21', end_node: false, node_when_true: 'yes', node_when_false: 'gender_rule' })
+CREATE (gender_rule:Rule { id: 'gender_rule', name: 'Over 18 and female', parameter_names: ['age', 'gender'], parameter_types:'int,String', expression:'(age >= 18) and gender == \\"female\\"', end_node: false, node_when_true: 'yes', node_when_false: 'no' })
+CREATE (answer_yes:Answer { id: 'yes', end_node: true, response: 'Pode entrar no bar'})
+CREATE (answer_no:Answer { id: 'no', end_node: true, response: 'Nao pode entrar no bar'})
 CREATE (tree)-[:HAS]->(over21_rule)
 CREATE (over21_rule)-[:IS_TRUE]->(answer_yes)
 CREATE (over21_rule)-[:IS_FALSE]->(gender_rule)
@@ -23,8 +22,9 @@ CREATE (gender_rule)-[:IS_FALSE]->(answer_no)
 query_ccg = """
 CREATE (tree:Tree { id: 'venda_nova' , next_node: 'quantidade_vidas', relationships: ['HAS']})
 
-CREATE (quantidade_vidas:Rule { id: 'quantidade_vidas', name: 'Quantidade de vidas?', parameter_names: 'qtde', 
-expression:'qtde >= 30', end_node: false, node_when_true: 'true', node_when_false: 'false'})
+CREATE (quantidade_vidas:Rule { id: 'quantidade_vidas', name: 'Quantidade de vidas?', 
+parameter_names: ['qtde'], expression:"qtde >= 30", end_node: false, 
+node_when_true: 'true', node_when_false: 'false'})
 
 CREATE (answer_true:Answer { id: 'true', end_node: true, response: 'é maior ou igual que 30'})
 CREATE (answer_false:Answer { id: 'false', end_node: true, response: 'é menor que 30'})
@@ -36,5 +36,6 @@ CREATE (quantidade_vidas)-[:IS_FALSE]->(answer_false)
 query_example = """
 MATCH (n) RETURN n
 """
+session.run(query1)
 session.run(query_ccg)
 
