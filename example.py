b5fa5a99cd06c3d619bf30f80a46f508bf1101e1
Cristhian Dias <heycristhian@pop-os.localdomain>
2021-02-25 16:55:04 -0300

    Finalizado versao 1 que gera query

--- example.py
+++ example.py
@@ -3,11 +3,18 @@ from neo4j import GraphDatabase
 
 class App:
     def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
-        self.node = {}
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+        self._node_ids = []
 
     def close(self):
-        self.driver.close()
+        self._driver.close()
+
+    @property
+    def node_ids(self):
+        return self._node_ids
+
+    def append_node_ids(self, node_id):
+        self._node_ids.append(node_id)
 
     @staticmethod
     def _find_node_by_id(tx, id):
@@ -21,19 +28,22 @@ class App:
         return result
 
     def execute_decision_tree(self, tree_id, facts):
-        with self.driver.session() as session:
+        with self._driver.session() as session:
             tree = session.read_transaction(self._find_node_by_id, tree_id)
+            self.append_node_ids(tree[0]['id'])
             if tree:
                 next_node = tree[0]['next_node']
                 node = session.read_transaction(self._find_node_by_id, next_node)
+                self.append_node_ids(node[0]['id'])
                 return self.decision_path(node[0], facts)
             return None
 
     def decision_path(self, node, facts):
-        with self.driver.session() as session:
+        with self._driver.session() as session:
             parameter_names = node['parameter_names']
 
             if node['end_node']:
+                self.create_final_query(self.node_ids)
                 return node['response']
 
             for param in parameter_names:
@@ -47,7 +57,30 @@ class App:
 
             result = session.read_transaction(self._find_node_by_id, id_node)
             result = [record for record in result]
+            self.append_node_ids(result[0]['id'])
             return self.decision_path(result[0], facts)
+
+    def create_final_query(self, node_ids):
+        with self._driver.session() as session:
+            quantity_node = len(self.node_ids)
+            query = "MATCH "
+
+            for index in range(quantity_node):
+                query += f"(n{index}), "
+
+            query = query[:-2] + " WHERE "
+
+            for index in range(quantity_node):
+                query += f"n{index}.id = '{node_ids[index]}' AND "
+
+            query = query[:-4] + "RETURN "
+
+            for index in range(quantity_node):
+                query += f"n{index}, "
+
+            query = query[:-2]
+
+            print(f'Query: {query}')
 
 
 def bar_test():
